01d948a654b7dc19ce84c1899007b0ee6d3b5266
SamuelMumford <60023499+SamuelMumford@users.noreply.github.com>
2020-09-17 22:14:04 -0700

    Update F0F1HallBar.py

--- F0F1HallBar.py
+++ F0F1HallBar.py
@@ -441,7 +441,7 @@ for i in range(0, runs):
         vals = vals*-1
         
     print('New Values for length of ' + str(l))
-    pred = Dyz*w/(Dz*l)
+    pred = Dyz*(w - 2*depth)/(Dz*l)
     
     a1.plot(pts, vals/pred, label = str(np.round(100*w)/100), color = copper(i))
 a1.plot(np.linspace(0, l, 100), np.ones(100), color = "black")
