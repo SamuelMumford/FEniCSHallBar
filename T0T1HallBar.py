04650e9b3ae1ddbfa657b37a68e54eaf3c096e8b
SamuelMumford <60023499+SamuelMumford@users.noreply.github.com>
2020-09-17 22:15:24 -0700

    Update T0T1HallBar.py

--- T0T1HallBar.py
+++ T0T1HallBar.py
@@ -436,7 +436,7 @@ for i in range(0, runs):
     mask = (rati < cutoff)
         
     print('New Values for length of ' + str(l))
-    pred = Dyz*w/(Dz*l)
+    pred = Dyz*(w - 2*depth)/(Dz*l)
     
     a1.plot(pts, vals/pred, label = str(np.round(100*w)/100), color = copper(i))
 a1.plot(np.linspace(0, cutoff, 100), np.ones(100), color = "black")
